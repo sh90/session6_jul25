c871cc56348e738f7a873f6ecc6d2118d9f9ac02
sh90 <shikhatyagi1990@gmail.com>
2025-07-27 20:29:57 +0530

    Create 2.google_search.py

---
+++ 2.google_search.py
@@ -0,0 +1,20 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+# options.add_argument("--headless")  # Run in headless mode (no UI)
+options.add_argument(
+    "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+
+# Setup WebDriver
+driver = webdriver.Chrome(options=options)
+# driver = webdriver.Firefox(options=options)
+url_val = "https://www.google.com"
+time.sleep(5)
+driver.get(url_val)
+time.sleep(5)
+driver.find_element("name", "q").send_keys("Selenium Python")
+#driver.find_element("name", "q").submit()
+time.sleep(5)
